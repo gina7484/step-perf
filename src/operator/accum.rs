96992aa83b1f471a9bdb1cfb3205549566146dcd
Gina Sohn <gina7484@gmail.com>
2025-07-01 01:10:12 +0000

    accum (retile row) tested

--- src/operator/accum.rs
+++ src/operator/accum.rs
@@ -288,4 +288,97 @@ mod tests {
             .unwrap()
             .run(Default::default());
     }
+
+    #[test]
+    fn test_retile_row() {
+        // [1,3,3] => [1,3]
+        // [1,4] tile => [3,4] tile
+        fn create_input_data(arrays: &[Array2<i32>], read_from_mu: bool) -> Vec<Elem<Tile<i32>>> {
+            let mut in_stream_data: Vec<Elem<Tile<i32>>> = Vec::new();
+            for (i, arr) in arrays.iter().enumerate() {
+                let tile = Tile::new(arr.clone().into(), 4, read_from_mu);
+
+                // Add ValStop at indices 2, 5, 8 (end of each row in 3x3 grid)
+                if i % 3 == 2 {
+                    if i == 8 {
+                        in_stream_data.push(Elem::ValStop(tile, 2));
+                    } else {
+                        in_stream_data.push(Elem::ValStop(tile, 1));
+                    }
+                } else {
+                    in_stream_data.push(Elem::Val(tile));
+                }
+            }
+            in_stream_data
+        }
+
+        fn create_ground_truth(arrays: &[Array2<i32>], read_from_mu: bool) -> Vec<Elem<Tile<i32>>> {
+            let mut ground_truth_data: Vec<Elem<Tile<i32>>> = Vec::new();
+            for i in 0..3 {
+                let concatenated_array = ndarray::concatenate(
+                    ndarray::Axis(0),
+                    &[
+                        arrays[i * 3].view(),
+                        arrays[i * 3 + 1].view(),
+                        arrays[i * 3 + 2].view(),
+                    ],
+                )
+                .unwrap_or_else(|_| {
+                    panic!("Failed to concatenate input data and accumulator data")
+                });
+
+                let elem = if i == 2 {
+                    Elem::ValStop(
+                        Tile::new(concatenated_array.to_shared(), 4, read_from_mu),
+                        1,
+                    )
+                } else {
+                    Elem::Val(Tile::new(concatenated_array.to_shared(), 4, read_from_mu))
+                };
+
+                ground_truth_data.push(elem);
+            }
+            ground_truth_data
+        }
+
+        // Step 1: Create 9 different ndarray::ArcArray2<T> with shape 2x2
+        let arrays: Vec<Array2<i32>> = (0..9)
+            .map(|i| Array2::from_shape_vec((1, 4), vec![i as i32; 4]).unwrap())
+            .collect();
+        let read_from_mu = true;
+        // Step 2: Create a 3x3 rank-2 data stream from these arrays
+        let in_stream_data = create_input_data(&arrays, read_from_mu);
+
+        // Step 3: Create a ground truth for the output stream
+        let ground_truth_data = create_ground_truth(&arrays, read_from_mu);
+
+        // Step 4: Create the STeP program
+        let mut ctx = ProgramBuilder::default();
+        let (in_data_snd, in_data_rcv) = ctx.unbounded();
+        let (out_data_snd, out_data_rcv) = ctx.unbounded();
+        ctx.add_child(GeneratorContext::new(
+            || in_stream_data.into_iter(),
+            in_data_snd,
+        ));
+        ctx.add_child(Accum::<SimpleEvent, _, _>::new(
+            in_data_rcv,
+            out_data_snd,
+            Arc::new(map_fn::retile_row),
+            Arc::new(move || Tile::new_empty([0, 4], read_from_mu)),
+            1, // rank
+            AccumConfig {
+                compute_bw: 1000, // FLOPs per cycle (Currently unused)
+                write_back_mu: true,
+            },
+            0, // id
+        ));
+        ctx.add_child(ApproxCheckerContext::new(
+            || ground_truth_data.into_iter(),
+            out_data_rcv,
+            tolerance_fn,
+        ));
+        ctx.initialize(Default::default())
+            .unwrap()
+            .run(Default::default());
+    }
 }
