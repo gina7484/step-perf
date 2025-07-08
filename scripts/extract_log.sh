b4306a251bbf43b5a8f6525d64e25e50b091c22d
Gina Sohn <gina7484@gmail.com>
2025-07-08 23:34:14 +0000

    updated for extracing mongodb logs easier for sweeps

--- scripts/extract_log.sh
+++ scripts/extract_log.sh
@@ -1,22 +1,35 @@
 # ======================= HBM Store =======================
 event_name="SimpleEvent"
-db_name="test_prefill_expert_mnk_mnk_0613"
-# event_name="WeightQLoad"
-# event_name="GenQ"
-# event_name="StoreOutput"
-mongosh --quiet ${db_name} --eval 'print("timestamp,name,id,start_ns,end_ns,is_stop"); db.log.aggregate([
-  { $match: { event_type: "'${event_name}'" } },
-  { $project: { 
-      _id: 0, 
-      timestamp: 1, 
-      name: "$event_data.name",
-      id: "$event_data.id", 
-      start_ns: "$event_data.start_ns", 
-      end_ns: "$event_data.end_ns",
-      is_stop: "$event_data.is_stop",
-    }
-  },
-  { $sort: { timestamp: 1 } }
-]).forEach(function(doc) { 
-  print(doc.timestamp + "," + doc.name + "," + doc.id + "," + doc.start_ns + "," + doc.end_ns + "," + doc.is_stop); 
-})' > data/${db_name}.csv
+
+db_names=(
+    # "expert_mn_mk_32_256_32_256"
+    # "expert_mn_mk_32_256_64_256"
+    # "expert_mn_mk_64_256_128_256"
+    "expert_mn_mk_64_256_16_256"
+    "expert_mn_mk_64_256_256_256"
+    "expert_mn_mk_64_256_32_256"
+    "expert_mn_mk_64_256_64_256"
+)
+
+
+# Loop through each database name
+for db_name in "${db_names[@]}"; do
+  echo "Processing database: ${db_name}"
+  mongosh --quiet ${db_name} --eval 'print("timestamp,name,id,start_ns,end_ns,is_stop"); db.log.aggregate([
+    { $match: { event_type: "'${event_name}'" } },
+    { $project: { 
+        _id: 0, 
+        timestamp: 1, 
+        name: "$event_data.name",
+        id: "$event_data.id", 
+        start_ns: "$event_data.start_ns", 
+        end_ns: "$event_data.end_ns",
+        is_stop: "$event_data.is_stop",
+      }
+    },
+    { $sort: { timestamp: 1 } }
+  ]).forEach(function(doc) { 
+    print(doc.timestamp + "," + doc.name + "," + doc.id + "," + doc.start_ns + "," + doc.end_ns + "," + doc.is_stop); 
+  })' > data/${db_name}.csv
+  echo "Exported data to data/${db_name}.csv"
+done
--- scripts/gantt_chart.sh
+++ scripts/gantt_chart.sh
@@ -1,5 +1,22 @@
-db_name="test_prefill_expert_mnk_mnk_0613"
+db_names=(
+    "expert_mn_mk_16_256_128_256"
+    "expert_mn_mk_16_256_256_256"
+    "expert_mn_mk_32_256_16_256"
+    "expert_mn_mk_32_256_32_256"
+    "expert_mn_mk_32_256_64_256"
+    "expert_mn_mk_64_256_128_256"
+    "expert_mn_mk_64_256_16_256"
+    "expert_mn_mk_64_256_256_256"
+    "expert_mn_mk_64_256_32_256"
+    "expert_mn_mk_64_256_64_256"
+)
 
-python scripts/gantt_chart_generator.py \
---csv_file data/${db_name}.csv \
---output_file gantt_chart_${db_name}.html
+# Loop through each database name
+for db_name in "${db_names[@]}"; do
+
+    python scripts/gantt_chart_generator.py \
+    --csv_file data/${db_name}.csv \
+    --output_file gantt_chart_${db_name}.html
+
+    echo "Generated gantt_chart_${db_name}.html"
+done
