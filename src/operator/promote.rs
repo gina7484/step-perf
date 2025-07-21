98995b7fb532bf86f22ad7d3b0c03a01d2d61735
Gina Sohn <gina7484@gmail.com>
2025-07-21 10:04:33 +0000

    revert promote to original version

--- src/operator/promote.rs
+++ src/operator/promote.rs
@@ -2,13 +2,13 @@ use crate::primitives::elem::{Elem, StopType};
 use dam::context_tools::*;
 
 #[context_macro]
-pub struct PromoteOld<T: DAMType> {
+pub struct Promote<T: DAMType> {
     in_stream: Receiver<Elem<T>>,
     out_stream: Sender<Elem<T>>,
     promote_rank: StopType,
 }
 
-impl<T: DAMType> PromoteOld<T>
+impl<T: DAMType> Promote<T>
 where
     Self: Context,
 {
@@ -29,7 +29,7 @@ where
     }
 }
 
-impl<T: DAMType> Context for PromoteOld<T> {
+impl<T: DAMType> Context for Promote<T> {
     fn run(&mut self) {
         loop {
             match self.in_stream.dequeue(&self.time) {
@@ -76,84 +76,6 @@ impl<T: DAMType> Context for PromoteOld<T> {
     }
 }
 
-#[context_macro]
-pub struct Promote<T: DAMType> {
-    in_stream: Receiver<Elem<T>>,
-    out_stream: Sender<Elem<T>>,
-    in_stream_rank: StopType,
-}
-
-impl<T: DAMType> Promote<T>
-where
-    Self: Context,
-{
-    pub fn new(
-        in_stream: Receiver<Elem<T>>,
-        out_stream: Sender<Elem<T>>,
-        in_stream_rank: StopType,
-    ) -> Self {
-        let ctx = Self {
-            in_stream,
-            out_stream,
-            in_stream_rank,
-            context_info: Default::default(),
-        };
-        ctx.in_stream.attach_receiver(&ctx);
-        ctx.out_stream.attach_sender(&ctx);
-        ctx
-    }
-}
-impl<T: DAMType> Context for Promote<T> {
-    fn run(&mut self) {
-        loop {
-            match self.in_stream.dequeue(&self.time) {
-                Ok(ChannelElement { time: _, data }) => match data {
-                    Elem::Val(x) => {
-                        let new_elem = if self.in_stream_rank == 0 {
-                            match self.in_stream.peek_next(&self.time) {
-                                Ok(ChannelElement { time: _, data: _ }) => Elem::Val(x),
-                                Err(_closed) => Elem::ValStop(x, 1),
-                            }
-                        } else {
-                            Elem::Val(x)
-                        };
-                        self.out_stream
-                            .enqueue(
-                                &self.time,
-                                ChannelElement {
-                                    time: self.time.tick(),
-                                    data: new_elem,
-                                },
-                            )
-                            .unwrap();
-                    }
-                    Elem::ValStop(x, s) => {
-                        let new_stop_rank = if self.in_stream_rank == s {
-                            match self.in_stream.peek_next(&self.time) {
-                                Ok(ChannelElement { time: _, data: _ }) => s,
-                                Err(_closed) => s + 1,
-                            }
-                        } else {
-                            s
-                        };
-
-                        self.out_stream
-                            .enqueue(
-                                &self.time,
-                                ChannelElement {
-                                    time: self.time.tick(),
-                                    data: Elem::ValStop(x, new_stop_rank),
-                                },
-                            )
-                            .unwrap();
-                    }
-                },
-                Err(_) => return,
-            };
-        }
-    }
-}
-
 #[cfg(test)]
 mod tests {
     use dam::{
@@ -163,10 +85,10 @@ mod tests {
 
     use crate::primitives::elem::Elem;
 
-    use super::{Promote, PromoteOld};
+    use super::Promote;
 
     #[test]
-    fn promote_old_2d() {
+    fn promote_2d() {
         let mut ctx = ProgramBuilder::default();
 
         let (in_snd, in_rcv) = ctx.unbounded();
@@ -193,7 +115,7 @@ mod tests {
             out_rcv,
             |x, y| x == y,
         ));
-        ctx.add_child(PromoteOld::new(in_rcv, out_snd, 2));
+        ctx.add_child(Promote::new(in_rcv, out_snd, 2));
 
         ctx.initialize(Default::default())
             .unwrap()
@@ -201,7 +123,7 @@ mod tests {
     }
 
     #[test]
-    fn promote_old_0d() {
+    fn promote_0d() {
         let mut ctx = ProgramBuilder::default();
 
         let (in_snd, in_rcv) = ctx.unbounded();
@@ -223,7 +145,7 @@ mod tests {
             out_rcv,
             |x, y| x == y,
         ));
-        ctx.add_child(PromoteOld::new(in_rcv, out_snd, 0));
+        ctx.add_child(Promote::new(in_rcv, out_snd, 0));
 
         ctx.initialize(Default::default())
             .unwrap()
@@ -231,7 +153,7 @@ mod tests {
     }
 
     #[test]
-    fn promote_old_0d_on_0d() {
+    fn promote_0d_on_0d() {
         let mut ctx = ProgramBuilder::default();
 
         let (in_snd, in_rcv) = ctx.unbounded();
@@ -250,64 +172,6 @@ mod tests {
                 ]
                 .into_iter()
             },
-            out_rcv,
-            |x, y| x == y,
-        ));
-        ctx.add_child(PromoteOld::new(in_rcv, out_snd, 0));
-
-        ctx.initialize(Default::default())
-            .unwrap()
-            .run(Default::default());
-    }
-
-    #[test]
-    fn promote_2d() {
-        let mut ctx = ProgramBuilder::default();
-
-        let (in_snd, in_rcv) = ctx.unbounded();
-        let (out_snd, out_rcv) = ctx.unbounded();
-
-        ctx.add_child(GeneratorContext::new(
-            || {
-                (0..4u32)
-                    .map(|x| Elem::Val(x))
-                    .chain(std::iter::once(Elem::ValStop(5, 1)))
-                    .chain((0..4u32).map(|x| Elem::Val(x)))
-                    .chain(std::iter::once(Elem::ValStop(5, 1)))
-            },
-            in_snd,
-        ));
-        ctx.add_child(ApproxCheckerContext::new(
-            || {
-                (0..4u32)
-                    .map(|x| Elem::Val(x))
-                    .chain(std::iter::once(Elem::ValStop(5, 1)))
-                    .chain((0..4u32).map(|x| Elem::Val(x)))
-                    .chain(std::iter::once(Elem::ValStop(5, 2)))
-            },
-            out_rcv,
-            |x, y| x == y,
-        ));
-        ctx.add_child(Promote::new(in_rcv, out_snd, 1));
-
-        ctx.initialize(Default::default())
-            .unwrap()
-            .run(Default::default());
-    }
-
-    #[test]
-    fn promote_0d() {
-        let mut ctx = ProgramBuilder::default();
-
-        let (in_snd, in_rcv) = ctx.unbounded();
-        let (out_snd, out_rcv) = ctx.unbounded();
-
-        ctx.add_child(GeneratorContext::new(
-            || vec![Elem::Val(0), Elem::Val(1), Elem::Val(2)].into_iter(),
-            in_snd,
-        ));
-        ctx.add_child(ApproxCheckerContext::new(
-            || vec![Elem::Val(0), Elem::Val(1), Elem::ValStop(2, 1)].into_iter(),
             out_rcv,
             |x, y| x == y,
         ));
