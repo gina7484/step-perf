e3a24dc240fcdc7013f4d0640f3c572c2a6e0127
Gina Sohn <gina7484@gmail.com>
2025-08-02 21:25:16 +0000

    added functions and tests required for attention

--- src/functions/map_fn.rs
+++ src/functions/map_fn.rs
@@ -193,6 +193,7 @@ pub fn add<T: Debug + ndarray::LinalgScalar + Default>(
 }
 
 // SiLU(x)= x / (1 + e^-x)
+// We will count this as 8 FLOPs per element
 pub fn silu<T: Debug + ndarray::LinalgScalar + num_traits::Float + Copy>(
     in_data: &Tile<T>,
     flop_per_cycle: u64,
@@ -207,7 +208,7 @@ pub fn silu<T: Debug + ndarray::LinalgScalar + num_traits::Float + Copy>(
 
     match &in_data.underlying {
         Some(arr) => (
-            div_ceil((shape_0 * shape_1) as u64, flop_per_cycle),
+            div_ceil((shape_0 * shape_1 * 8) as u64, flop_per_cycle),
             Tile::new_padded(
                 arr.mapv(|x| x / (T::one() + (-x).exp())).to_shared(),
                 in_data.bytes_per_elem,
@@ -216,7 +217,7 @@ pub fn silu<T: Debug + ndarray::LinalgScalar + num_traits::Float + Copy>(
             ),
         ),
         None => (
-            div_ceil((shape_0 * shape_1) as u64, flop_per_cycle),
+            div_ceil((shape_0 * shape_1 * 8) as u64, flop_per_cycle),
             Tile::new_blank_padded(
                 vec![shape_0, shape_1],
                 in_data.bytes_per_elem,
@@ -224,5 +225,195 @@ pub fn silu<T: Debug + ndarray::LinalgScalar + num_traits::Float + Copy>(
                 offset,
             ),
         ),
+    }
+}
+
+// exp(x) (~ 4 FLOPs per element)
+pub fn exp<T: Debug + num_traits::Float + Copy>(
+    in_data: &Tile<T>,
+    flop_per_cycle: u64,
+    write_back_mu: bool,
+) -> (u64, Tile<T>) {
+    assert_eq!(in_data.shape.len(), 2);
+
+    let shape_0 = in_data.shape[0];
+    let shape_1 = in_data.shape[1];
+
+    let offset = in_data.offset;
+
+    match &in_data.underlying {
+        Some(arr) => (
+            div_ceil((shape_0 * shape_1 * 4) as u64, flop_per_cycle),
+            Tile::new_padded(
+                arr.mapv(|x| x.exp()).to_shared(),
+                in_data.bytes_per_elem,
+                write_back_mu,
+                offset,
+            ),
+        ),
+        None => (
+            div_ceil((shape_0 * shape_1 * 4) as u64, flop_per_cycle),
+            Tile::new_blank_padded(
+                vec![shape_0, shape_1],
+                in_data.bytes_per_elem,
+                write_back_mu,
+                offset,
+            ),
+        ),
+    }
+}
+
+pub fn row_wise_sum<T: Debug + num_traits::Num + Copy>(
+    in_data: &Tile<T>,
+    flop_per_cycle: u64,
+    write_back_mu: bool,
+) -> (u64, Tile<T>) {
+    assert_eq!(in_data.shape.len(), 2);
+
+    let shape_0 = in_data.shape[0];
+    let shape_1 = in_data.shape[1];
+
+    let offset = in_data.offset;
+
+    match &in_data.underlying {
+        Some(arr) => {
+            // Perform row-wise sum: sum each row to get a [shape_0, 1] array
+            let row_sums = arr.sum_axis(ndarray::Axis(1)).insert_axis(ndarray::Axis(1));
+            (
+                div_ceil((shape_0 * shape_1) as u64, flop_per_cycle),
+                Tile::new_padded(
+                    row_sums.to_shared(),
+                    in_data.bytes_per_elem,
+                    write_back_mu,
+                    offset,
+                ),
+            )
+        }
+        None => (
+            div_ceil((shape_0 * shape_1) as u64, flop_per_cycle),
+            Tile::new_blank_padded(
+                vec![shape_0, 1],
+                in_data.bytes_per_elem,
+                write_back_mu,
+                offset,
+            ),
+        ),
+    }
+}
+
+pub fn set_offset<T: Debug + ndarray::LinalgScalar + Default>(
+    in_data: &Tile<T>,
+    offset: u64,
+) -> (u64, Tile<T>) {
+    assert_eq!(in_data.shape.len(), 2);
+    let shape_0 = in_data.shape[0];
+    let shape_1 = in_data.shape[1];
+
+    match &in_data.underlying {
+        Some(arr) => (
+            1,
+            Tile::new_padded(
+                arr.to_owned().into_shared(),
+                in_data.bytes_per_elem,
+                in_data.read_from_mu,
+                offset as usize,
+            ),
+        ),
+        None => (
+            1,
+            Tile::new_blank_padded(
+                vec![shape_0, shape_1],
+                in_data.bytes_per_elem,
+                in_data.read_from_mu,
+                offset as usize,
+            ),
+        ),
+    }
+}
+
+pub fn row_wise_append<T: Debug + Default + Clone>(
+    in_data: &Tile<T>,
+    data_to_append: &Tile<T>,
+) -> (u64, Tile<T>) {
+    assert_eq!(in_data.shape.len(), 2);
+    assert_eq!(data_to_append.shape.len(), 2);
+    assert_eq!(in_data.shape[1], data_to_append.shape[1]);
+
+    let shape_0 = in_data.shape[0];
+    let shape_1 = in_data.shape[1];
+
+    let offset = in_data.offset;
+    assert!(
+        offset + data_to_append.shape[0] <= shape_0,
+        "should have enough space to append new rows"
+    );
+
+    match (&in_data.underlying, &data_to_append.underlying) {
+        (Some(arr), Some(arr_to_append)) => {
+            // Create a mutable copy of the original array
+            let mut result = arr.to_owned();
+
+            // Copy data from arr_to_append into the slice starting at offset
+            let mut slice =
+                result.slice_mut(ndarray::s![offset..offset + data_to_append.shape[0], ..]);
+            slice.assign(arr_to_append);
+
+            (
+                1,
+                Tile::new_padded(
+                    result.into_shared(),
+                    in_data.bytes_per_elem,
+                    in_data.read_from_mu,
+                    (offset + data_to_append.shape[0]) as usize,
+                ),
+            )
+        }
+        _ => (
+            1,
+            Tile::new_blank_padded(
+                vec![shape_0, shape_1],
+                in_data.bytes_per_elem,
+                in_data.read_from_mu,
+                (offset + data_to_append.shape[0]) as usize,
+            ),
+        ),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_row_wise_sum() {
+        let arr = ndarray::Array2::from_shape_fn((3, 4), |(i, j)| i as f32 + j as f32);
+        println!("input arr: {:?}", arr);
+        let in_data = Tile::new_padded(arr.to_shared(), 4, false, 3);
+        let (flop_count, out_data) = row_wise_sum(&in_data, 6, false);
+
+        println!("output arr: {:?}", out_data.underlying.unwrap());
+        assert_eq!(flop_count, 2);
+    }
+
+    #[test]
+    fn test_row_wise_append() {
+        let arr =
+            ndarray::Array2::from_shape_fn(
+                (6, 4),
+                |(i, j)| if i < 3 { i as f32 + j as f32 } else { 0.0 },
+            );
+        println!("input arr: {:?}", arr);
+        let in_data = Tile::new_padded(arr.to_shared(), 4, false, 3);
+        let arr_to_append = ndarray::Array2::from_shape_fn((1, 4), |(i, j)| 3 as f32 + j as f32);
+        let data_to_append = Tile::new_padded(arr_to_append.to_shared(), 4, false, 1);
+        println!(
+            "data_to_append: {:?}",
+            data_to_append.underlying.as_ref().unwrap()
+        );
+
+        let (flop_count, out_data) = row_wise_append(&in_data, &data_to_append);
+        println!("output arr: {:?}", out_data.underlying.as_ref().unwrap());
+        assert_eq!(out_data.offset, 4);
+        assert_eq!(flop_count, 1);
     }
 }
