049acc27ff2ae55f3d0b48a4c9a8efe73cedaca0
Gina Sohn <gina7484@gmail.com>
2025-12-13 11:14:53 +0000

    add mem config for hdl sim

---
+++ src/memory/hdl_mod.rs
@@ -0,0 +1,18 @@
+pub mod dyn_offchip_load;
+pub mod metadata_gen;
+pub mod offchip_load;
+pub mod offchip_store;
+pub mod random_offchip_load;
+pub mod random_offchip_store;
+
+/// PMU bandwidth (bytes/cycle)
+pub static PMU_BW: u64 = 256;
+
+use crate::primitives::{elem::StopType, tile::Tile};
+use dam::types::DAMType;
+
+#[derive(Debug)]
+pub enum HbmAddrEnum<T: DAMType> {
+    ADDR(Vec<u64>, Tile<T>),
+    ADDRSTOP(Vec<u64>, Tile<T>, StopType),
+}
