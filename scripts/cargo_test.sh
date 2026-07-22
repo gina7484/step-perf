0939c48f84922f3a23aa9a20b6656f2f4789d1b4
Gina Sohn <gina7484@gmail.com>
2026-07-21 21:46:26 -0700

    paths

--- scripts/cargo_test.sh
+++ scripts/cargo_test.sh
@@ -1,2 +1,3 @@
 RUST_BACKTRACE=1 cargo test --package step_perf --lib -- test::protobuf_test::test::run_graph --exact --show-output
 # cargo test --package step_perf --lib -- test::protobuf_test::test::run_graph --exact --show-output
+# cargo test --package step_perf --lib -- operator::reassemble::tests -- --show-output
