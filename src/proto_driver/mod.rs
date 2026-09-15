b7a3d34cf979f32d396275e389eb38aca3a2b842
Gina Sohn <gina7484@gmail.com>
2026-09-14 18:25:09 -0700

    add more type

--- src/proto_driver/mod.rs
+++ src/proto_driver/mod.rs
@@ -1409,6 +1409,41 @@ fn build_from_proto<'a>(
                             operation.id,
                         ));
                     }
+                    (Type::U64(_), Type::F32(_)) => {
+                        let in_rcv = channel_map_collection.tile_u64.get_receiver(
+                            repeat_ref.input_id,
+                            repeat_ref.input_stream_idx,
+                            builder,
+                            get_chan_depth(
+                                &sim_config.config_dict,
+                                repeat_ref.input_id,
+                                channel_depth,
+                            ),
+                        );
+                        let ref_rcv = channel_map_collection.tile_f32.get_receiver(
+                            repeat_ref.ref_id,
+                            repeat_ref.ref_stream_idx,
+                            builder,
+                            get_chan_depth(
+                                &sim_config.config_dict,
+                                repeat_ref.ref_id,
+                                channel_depth,
+                            ),
+                        );
+                        let snd = channel_map_collection.tile_u64.get_sender(
+                            operation.id,
+                            None,
+                            builder,
+                            get_chan_depth(&sim_config.config_dict, operation.id, channel_depth),
+                        );
+                        builder.add_child(RepeatRef::<_, _>::new(
+                            in_rcv,
+                            ref_rcv,
+                            snd,
+                            repeat_ref.rank,
+                            operation.id,
+                        ));
+                    }
                     (Type::U64(_), Type::U64(_)) => {
                         let in_rcv = channel_map_collection.tile_u64.get_receiver(
                             repeat_ref.input_id,
